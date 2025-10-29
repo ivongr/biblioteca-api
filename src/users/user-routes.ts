9fa7b5b3c79cf9f62ee11619ed79617486392009
Ivon <ivongarcia@siticl.com>
2025-10-29 12:23:50 -0600

    refacotr: agrega try-catch en los endpoint de users

--- src/users/user-routes.ts
+++ src/users/user-routes.ts
@@ -5,15 +5,22 @@ import { deleteUser } from './uses-cases/delete-user.ts';
 import { getUsers } from './uses-cases/get-users.ts';
 import { saveUser } from './uses-cases/save-user.ts';
 import { updateUser } from './uses-cases/update-user.ts';
-import { message } from 'valibot';
 
 export const userRoutes = Router();
 const ROUTE = '/users';
 
 userRoutes.get(ROUTE, async (_, res) => {
-  return getUsers().then((resp) => {
-    res.json(resp).status(HttpStatusCodes.OK);
-  });
+  try {
+    const users = await getUsers();
+    res.status(HttpStatusCodes.OK).json({
+      data: users,
+    });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : 'Error desconocido';
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
+      message,
+    });
+  }
 });
 
 userRoutes.post(ROUTE, async (req, res) => {
@@ -21,7 +28,6 @@ userRoutes.post(ROUTE, async (req, res) => {
     const user = await saveUser({ user: req.body });
 
     res.status(HttpStatusCodes.OK).json({
-      success: true,
       message: 'Usuario creado correctamente',
       data: user,
     });
@@ -32,13 +38,31 @@ userRoutes.post(ROUTE, async (req, res) => {
 });
 
 userRoutes.put(ROUTE, async (req, res) => {
-  return updateUser({ user: req.body }).then((resp) => {
-    res.json(resp).status(HttpStatusCodes.OK);
-  });
+  try {
+    const user = await updateUser({ user: req.body });
+    res.status(HttpStatusCodes.OK).json({
+      message: 'Usuario actualizado correctamente',
+      data: user,
+    });
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : 'Error desconocido';
+    res.status(HttpStatusCodes.BAD_REQUEST).json({
+      message,
+    });
+  }
 });
 
 userRoutes.delete(ROUTE + '/:id', async (req, res) => {
-  return deleteUser({ id: req.params.id }).then((resp) => {
-    res.json(resp).status(HttpStatusCodes.OK);
-  });
+  try {
+    const book = await deleteUser({ id: req.params.id });
+    res.status(HttpStatusCodes.OK).json({
+      message: 'Usuario eliminado correctamente',
+      data: book,
+    });
+  } catch (err) {
+    const message = err instanceof Error ? err.message : 'Error desconocido';
+    res.status(HttpStatusCodes.BAD_REQUEST).json({
+      message,
+    });
+  }
 });
