ac34ae0faadbb9ca3cff9354f03e0ef53057322b
Ivon <ivongarcia@siticl.com>
2025-10-29 00:01:38 -0600

    feat: valida cuando un usuario no existe o si ese usuario ya tiene ese libro

--- src/books/uses-cases/save-book.ts
+++ src/books/uses-cases/save-book.ts
@@ -1,16 +1,43 @@
+import { ObjectId } from 'mongodb';
+
 import { dbBiblioteca } from '../../config/connect-db.ts';
 import { COLLECTIONS } from '../../shared/constants/db-collections.ts';
 import { IBook } from '../entities/books.ts';
 import { parseBook } from '../validations/book-schema.ts';
 
-export async function saveBook(props: { user: IBook }): Promise<IBook> {
-  const parsedBook = parseBook(props.user);
+export async function saveBook(props: { book: IBook }): Promise<IBook> {
+  const parsedBook = parseBook(props.book);
 
-  const result = await dbBiblioteca.collection(COLLECTIONS.books).insertOne(parsedBook);
+  const userExists = await dbBiblioteca
+    .collection(COLLECTIONS.users)
+    .findOne({ _id: new ObjectId(parsedBook.idUser) });
+
+  if (!userExists) {
+    throw new Error('Usuario no encontrado');
+  }
+
+  const existingBook = await dbBiblioteca.collection<IBook>(COLLECTIONS.books).findOne({
+    idUser: new ObjectId(parsedBook.idUser),
+    title: parsedBook.title,
+    autor: parsedBook.autor,
+  });
+
+  if (existingBook) {
+    throw new Error('Este libro ya existe para este usuario');
+  }
+
+  const saveBook = {
+    title: parsedBook.title,
+    autor: parsedBook.autor,
+    idUser: new ObjectId(parsedBook.idUser),
+  };
+
+  const result = await dbBiblioteca.collection(COLLECTIONS.books).insertOne(saveBook);
 
   return {
     _id: result.insertedId.toString(),
     autor: parsedBook.autor,
     title: parsedBook.title,
+    idUser: parsedBook.idUser,
   };
 }
